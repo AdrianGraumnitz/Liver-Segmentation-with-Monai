95b34178e2ab19407a9caf38f6f0ee4f5bb00cab
Graumnitz <AGraumnitz@o365.comhard.de>
2023-10-05 08:02:52 +0200

    I added a function to preprocess and handle training data. Additionally, I organized my UNet architecture in a separate file for better code structure and modularity.

---
+++ net.py
@@ -0,0 +1,16 @@
+from monai.networks.layers import Norm
+import torch
+from monai.networks.nets import UNet
+
+def unet():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = UNet(
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=2,
+        channels=(16, 32, 64, 128, 256), 
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+        norm=Norm.BATCH,
+    ).to(device)
+    return model, device
